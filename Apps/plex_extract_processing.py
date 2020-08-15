3061855b3d791fccc32419232dce17be53497a27
Dick Kluis <dkluis@icloud.com>
2020-08-15 17:22:12 -0400

    Hacking Plex to get the watched episodes to update TVM-Management and thus TVMaze

--- Apps/plex_extract_processing.py
+++ Apps/plex_extract_processing.py
@@ -23,63 +23,9 @@ import os
 from db_lib import execute_sql
 
 
-class sdb_info:
-    check = os.getcwd()
-    if 'Pycharm' in check:
-        data = '/Volumes/HD-Data-CA-Server/PlexMedia/PlexProcessing/Plex DB/com.plexapp.plugins.library.db'
-    else:
-        data = '/Users/dick/Library/Application Support/Plex Media Server/' \
-               'Plug-in Support/Databases/com.plexapp.plugins.library.db'
-
-
-def connect_sdb():
-    try:
-        sdb = sqlite3.connect(sdb_info.data)
-    except sqlite3.Error as err:
-        print(f'Error Opening the DB with error {err} on {sdb_info.data}')
-        quit()
-    scur = sdb.cursor()
-    sdict = {'sdb': sdb,
-             'scursor': scur}
-    return sdict
-
-
-def close_sdb(sdb):
-    sdb.close()
-
-
-def execute_sqlite(sqltype='', sql=''):
-    sdb = connect_sdb()
-    sdbcur = sdb['scursor']
-    sdbdb = sdb['sdb']
-    if sqltype == 'Commit':
-        try:
-            sdbcur.execute(sql)
-            sdbdb.commit()
-        except sqlite3.Error as er:
-            print('Commit Database Error: ', er, sql)
-            print('----------------------------------------------------------------------')
-            close_sdb(sdbdb)
-            return False, er
-        close_sdb(sdbdb)
-        return True
-    elif sqltype == "Fetch":
-        try:
-            sdbcur.execute(sql)
-            result = sdbcur.fetchall()
-        except sqlite3.Error as er:
-            print('Execute SQL Database Error: ', er, sql)
-            print('----------------------------------------------------------------------')
-            close_sdb(sdbdb)
-            return False, er
-        close_sdb(sdbdb)
-        return result
-    else:
-        return False, 'Not implemented yet'
-
-
 def fix_showname(sn):
     sn = sn.replace(" : ", " ").replace("vs.", "vs").replace("'", "").replace(":", '').replace("&", "and")
+    sn = sn.replace('"', '')
     if sn[-1:] == " ":
         sn = sn[:-1]
     lsix = sn[-6:]
@@ -96,6 +42,25 @@ def fix_showname(sn):
     return sn
 
 
+def update_plex_shows(psn, sn, si):
+    f_sql = f"select * from plex_shows where showname = {psn}"
+    recs = execute_sql(sqltype='Fetch', sql=f_sql)
+    if len(recs) == 0:
+        if vli > 3:
+            print(f'Insert a new records into Plex Shows for {psn}')
+        if si == 0:
+            w_sql = f"insert into plex_shows values ({psn}, None, '{sn}')"
+        else:
+            w_sql = f"insert into plex_shows values ({psn}, {si}, '{sn}')"
+        execute_sql(sqltype='Commit', sql=w_sql.replace("None", "NULL"))
+
+
+def find_show(ssn):
+    s_sql = f"select showid from shows where showname = '{ssn}' or alt_showname = '{ssn}'"
+    records = execute_sql(sqltype='Fetch', sql=s_sql)
+    return records
+
+
 ''' Main Program'''
 ''' Get Options'''
 options = docopt(__doc__, version='Plex Extract Release 0.1')
@@ -107,26 +72,6 @@ elif vli > 1:
     print(f'Verbosity level is set to: {options["--vl"]}')
 
 '''Get Plex Watched Episodes'''
-if options['-a']:
-    if vli > 1:
-        print(f'Getting all Plex Watched Episodes')
-    sqlw = f"select grandparent_title, parent_index, `index`, viewed_at " \
-           f"from metadata_item_views " \
-           f"where parent_index > 0 and metadata_type = 4 " \
-           f"order by grandparent_title asc, parent_index asc, `index` asc"
-else:
-    if vli > 1:
-        print(f'Getting Plex Watched Episodes since yesterday')
-    sqlw = f"select grandparent_title, parent_index, `index`, viewed_at " \
-           f"from metadata_item_views " \
-           f"where parent_index > 0 and metadata_type = 4 and viewed_at > date('now', '-1 day') " \
-           f"order by grandparent_title asc, parent_index asc, `index` asc"
-
-watched_episodes = execute_sqlite(sqltype='Fetch', sql=sqlw)
-if not watched_episodes:
-    print(f'Reading Plex DB while trying to get the watched episodes {watched_episodes}')
-    quit()
-
 check = os.getcwd()
 if 'Pycharm' in check:
     wetxt = '/Volumes/HD-Data-CA-Server/PlexMedia/PlexProcessing/TVMaze/Data/Plex_Watched_Episodes.txt'
@@ -134,17 +79,29 @@ else:
     wetxt = '/Volumes/HD-Media-CA-Media/PlexMedia/PlexProcessing/TVMaze/Data/Plex_Watched_Episodes.txt'
 
 try:
-    we = open(wetxt, "w")
+    we = open(wetxt, "r")
 except IOError as err:
     print(f'Error Opening the txt file {err}')
     quit()
 ew = 0
-for episode in watched_episodes:
-    we.write(f'{str(episode)}\n')
+for episode in we:
     ew += 1
+    episode = str(episode).replace('(', "").replace(')', '')
+    epi = str((episode[:-1])).split(',')
+    plex_sn = epi[0]
+    plex_season = epi[1]
+    plex_epi = epi[2]
+    plex_watch_date = epi[3]
+    fsn = fix_showname(plex_sn)
+    found_show = find_show(fsn)
+    if not found_show:
+        found_show = [(None,)]
+    if vli > 3:
+        print(f'Found showid {found_show[0][0]} for {fsn}')
+    update_plex_shows(plex_sn, fsn, found_show[0][0])
     if vli > 2:
-        print(f'Processed {episode}')
-
+        print(f'Processed {plex_sn}, {fsn}, {plex_season}, {plex_epi}, {plex_watch_date}')
+    
 we.close()
 if vli > 1:
-    print(f'Found Plex Episodes {len(watched_episodes)} and written {ew} to file')
+    print(f'Found Plex Episodes {ew} in the extract')
