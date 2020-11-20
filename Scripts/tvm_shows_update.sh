bea8cfae1e5d6a22ec32845cd590231a8a4da7f7
Dick Kluis <dkluis@icloud.com>
2020-11-20 08:56:56 -0500

    Changed the shows_update to allow for selections of followed, none followed or all.   Will add this into cron for a monthly update of show info for the whole DB

---
+++ Scripts/tvm_shows_update.sh
@@ -0,0 +1,28 @@
+### Setup zshell for Error Checking and Handling
+## Setup catching variables that are not initialized
+#
+set -o nounset
+## Setup catching Exit errors and stopping the script
+#
+set -o errexit
+
+AppDir='/Volumes/HD-Data-CA-Server/PlexMedia/PlexProcessing/TVMaze/Apps'
+LogDir='/Volumes/HD-Data-CA-Server/PlexMedia/PlexProcessing/TVMaze/Logs'
+
+### Spawning the next shell script, so that it can be tested and run by cron
+##
+#
+
+# Position in the temp directory
+cd $AppDir
+
+PYTHONPATH=/Library/Frameworks/Python.framework/Versions/3.8/lib/python3.8/site-packages
+export PYTHONPATH
+PATH=/Library/Frameworks/Python.framework/Versions/3.8/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin
+export PATH
+
+echo "$(date) >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+python3 shows_update.py -a --vl=5
+echo ""
+echo "$(Date) <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+echo ""
