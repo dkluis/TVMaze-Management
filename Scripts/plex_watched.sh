c76e4259704e855bd3e29466a05645dbff2921ec
Dick Kluis <dkluis@icloud.com>
2020-11-23 21:56:05 -0500

    This should be it, going to prod.

--- Scripts/plex_watched.sh
+++ Scripts/plex_watched.sh
@@ -1,5 +1,8 @@
 cd /Volumes/SharedFolders/PlexMedia/PlexProcessing/TVMaze/Apps
+echo " "
 echo $(date) "Starting Plex Watch Status Extract >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-/usr/local/bin/python3 plex_extract.py -p -w --vl=5
+echo " "
+/usr/local/bin/python3 plex_extract.py -p -w --vl=2
+echo " "
 echo $(date) "Finished with Plex Cleanup >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo " "
