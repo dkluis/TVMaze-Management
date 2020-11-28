6532811f61eb68784132efa4d66f2402c0cf71d8
Dick Kluis <dkluis@icloud.com>
2020-11-27 21:07:29 -0500

    Logging for Cleanup

--- Scripts/plex_cleanup.sh
+++ Scripts/plex_cleanup.sh
@@ -1,10 +1,6 @@
 
 
 cd /Volumes/HD-Data-CA-Server/PlexMedia/PlexProcessing/TVMaze/Apps
-echo $(date) "Starting Plex Cleanup"
 
 python3 plex_cleanup.py
 
-echo $(date) "Finished with Plex Cleanup"
-echo " "
-echo " "
